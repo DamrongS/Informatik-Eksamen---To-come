7d7968bca82d9ec135dbbb089fc49349bf2f2c21
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2025-03-17 08:47:51 +0100

    tjest

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let TextInput;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  TextInput = createInput().attribute("placeholder", "enter text to hash")
+  TextInput.position(width/2 - 10, 50)
 }
 
 function draw() {
   background(220);
+
+  text(TextInput.value(), width/2, 135);
+
+  text(TextInput.value().hashEncode(), width/2, 200);
+
 }
